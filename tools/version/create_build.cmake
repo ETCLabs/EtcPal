1d68ba9716a4bdd5d6119b848b4daa2f0c9e4d61
Sam Kearney <samuelmkearney@gmail.com>
2019-04-18 14:08:28 -0500

    Add version file and versioned build tools

---
+++ tools/version/create_build.cmake
@@ -0,0 +1,36 @@
+if(NOT NEW_VERSION_NUMBER)
+  message(FATAL_ERROR "You must pass a version number!")
+endif()
+
+set(LWPA_VERSION_STRING ${NEW_VERSION_NUMBER})
+string(REPLACE "." ";" LWPA_VERSION_LIST ${LWPA_VERSION_STRING})
+
+# Build the variables we will need to configure the version header
+list(GET LWPA_VERSION_LIST 0 LWPA_VERSION_MAJOR)
+list(GET LWPA_VERSION_LIST 1 LWPA_VERSION_MINOR)
+list(GET LWPA_VERSION_LIST 2 LWPA_VERSION_PATCH)
+list(GET LWPA_VERSION_LIST 3 LWPA_VERSION_BUILD)
+string(TIMESTAMP LWPA_VERSION_DATESTR "%Y-%m-%d")
+string(TIMESTAMP CURRENT_YEAR "%Y")
+set(LWPA_VERSION_COPYRIGHT "Copyright ${CURRENT_YEAR} ETC Inc.")
+
+# Configure the version header
+message(STATUS "Configuring versioned build for ${LWPA_VERSION_STRING}...")
+get_filename_component(VERSION_DIR ${CMAKE_SCRIPT_MODE_FILE} DIRECTORY)
+configure_file(${VERSION_DIR}/templates/version.h.in ${VERSION_DIR}/../../include/lwpa/version.h)
+configure_file(${VERSION_DIR}/templates/current_version.txt.in ${VERSION_DIR}/current_version.txt)
+
+# Stage the changed files
+execute_process(COMMAND
+  git add current_version.txt
+  WORKING_DIRECTORY ${VERSION_DIR}
+)
+execute_process(COMMAND
+  git add include/lwpa/version.h
+  WORKING_DIRECTORY ${VERSION_DIR}/../..
+)
+configure_file(${VERSION_DIR}/templates/commit_msg.txt.in ${VERSION_DIR}/tmp_commit_msg.txt)
+
+message(STATUS "Versioned files updated. Now commit and push your changes to do the build, e.g.:")
+message(STATUS "    git commit -F tmp_commit_msg.txt")
+message(STATUS "    git push origin")
