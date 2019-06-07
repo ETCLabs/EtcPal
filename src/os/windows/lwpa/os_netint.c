2b30323987cc6a796647a2e6bdee342d825f5d86
Sam Kearney <samuelmkearney@gmail.com>
2019-06-07 13:10:09 -0500

    Cleanup windows side

--- src/os/windows/lwpa/os_netint.c
+++ src/os/windows/lwpa/os_netint.c
@@ -37,16 +37,23 @@ static struct LwpaNetintState
 
   size_t num_netints;
   LwpaNetintInfo* lwpa_netints;
-  bool have_default_netint_index;
-  size_t default_netint_index;
+
+  bool have_default_netint_index_v4;
+  size_t default_netint_index_v4;
+
+  bool have_default_netint_index_v6;
+  size_t default_netint_index_v6;
 } state;
 
 /*********************** Private function prototypes *************************/
 
 static IP_ADAPTER_ADDRESSES* get_windows_adapters();
-static void copy_ipv4_info(IP_ADAPTER_UNICAST_ADDRESS* pip, IP_ADAPTER_GATEWAY_ADDRESS* pgate, LwpaNetintInfo* info);
+static void copy_ipv4_info(IP_ADAPTER_UNICAST_ADDRESS* pip, LwpaNetintInfo* info);
+static void copy_ipv6_info(IP_ADAPTER_UNICAST_ADDRESS* pip, LwpaNetintInfo* info);
 static void copy_all_netint_info(IP_ADAPTER_ADDRESSES* adapters);
+
 static lwpa_error_t enumerate_netints();
+static int compare_netints(const void* a, const void* b);
 static void free_netints();
 
 /*************************** Function definitions ****************************/
@@ -74,12 +81,12 @@ void lwpa_netint_deinit()
 
 size_t lwpa_netint_get_num_interfaces()
 {
-  return state.num_netints;
+  return (state.initialized ? state.num_netints : 0);
 }
 
 size_t lwpa_netint_get_interfaces(LwpaNetintInfo* netint_arr, size_t netint_arr_size)
 {
-  if (!netint_arr || netint_arr_size == 0)
+  if (!state.initialized || !netint_arr || netint_arr_size == 0)
     return 0;
 
   size_t addrs_copied = (netint_arr_size < state.num_netints ? netint_arr_size : state.num_netints);
@@ -87,23 +94,35 @@ size_t lwpa_netint_get_interfaces(LwpaNetintInfo* netint_arr, size_t netint_arr_
   return addrs_copied;
 }
 
-bool lwpa_netint_get_default_interface(LwpaNetintInfo* netint)
+bool lwpa_netint_get_default_interface(lwpa_iptype_t type, LwpaNetintInfo* netint)
 {
-  if (netint && state.have_default_netint_index)
+  if (state.initialized && netint)
   {
-    *netint = state.lwpa_netints[state.default_netint_index];
-    return true;
+    if (type == kLwpaIpTypeV4 && state.have_default_netint_index_v4)
+    {
+      *netint = state.lwpa_netints[state.default_netint_index_v4];
+      return true;
+    }
+    else if (type == kLwpaIpTypeV6 && state.have_default_netint_index_v6)
+    {
+      *netint = state.lwpa_netints[state.default_netint_index_v6];
+      return true;
+    }
   }
   return false;
 }
 
 lwpa_error_t lwpa_netint_get_interface_for_dest(const LwpaIpAddr* dest, LwpaNetintInfo* netint)
 {
-  const LwpaNetintInfo* res = NULL;
-  const LwpaNetintInfo* def = NULL;
-
   if (!dest || !netint)
     return kLwpaErrInvalid;
+  if (!state.initialized)
+    return kLwpaErrNotInit;
+  if (state.num_netints == 0)
+    return kLwpaErrNoNetints;
+
+  const LwpaNetintInfo* res = NULL;
+  const LwpaNetintInfo* def = NULL;
 
   for (const LwpaNetintInfo* netint_entry = state.lwpa_netints; netint_entry < state.lwpa_netints + state.num_netints;
        ++netint_entry)
@@ -150,11 +169,10 @@ lwpa_error_t enumerate_netints()
       switch (pip->Address.lpSockaddr->sa_family)
       {
         case AF_INET:
+        case AF_INET6:
           ++state.num_netints;
           break;
-        case AF_INET6:
         default:
-          // TODO IPv6
           break;
       }
       pip = pip->Next;
@@ -177,7 +195,35 @@ lwpa_error_t enumerate_netints()
 
   copy_all_netint_info(padapters);
   free(padapters);
+
+  // Sort the interfaces by OS index
+  qsort(state.lwpa_netints, state.num_netints, sizeof(LwpaNetintInfo), compare_netints);
+
+  // Store the locations of the default netints for access by the API function
+  for (size_t i = 0; i < state.num_netints; ++i)
+  {
+    LwpaNetintInfo* netint = &state.lwpa_netints[i];
+    if (LWPA_IP_IS_V4(&netint->addr) && netint->is_default)
+    {
+      state.have_default_netint_index_v4 = true;
+      state.default_netint_index_v4 = i;
+    }
+    else if (LWPA_IP_IS_V6(&netint->addr) && netint->is_default)
+    {
+      state.have_default_netint_index_v6 = true;
+      state.default_netint_index_v6 = i;
+    }
+  }
+
   return kLwpaErrOk;
+}
+
+int compare_netints(const void* a, const void* b)
+{
+  LwpaNetintInfo* netint1 = (LwpaNetintInfo*)a;
+  LwpaNetintInfo* netint2 = (LwpaNetintInfo*)b;
+
+  return (netint1->ifindex > netint2->ifindex) - (netint1->ifindex < netint2->ifindex);
 }
 
 void free_netints()
@@ -192,7 +238,7 @@ void free_netints()
 IP_ADAPTER_ADDRESSES* get_windows_adapters()
 {
   ULONG buflen = 0;
-  ULONG flags = GAA_FLAG_SKIP_ANYCAST | GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_DNS_SERVER | GAA_FLAG_INCLUDE_GATEWAYS;
+  ULONG flags = GAA_FLAG_SKIP_ANYCAST | GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_DNS_SERVER;
 
   // Preallocating a buffer specifically this size is expressly recommended by the Microsoft usage
   // page.
@@ -213,64 +259,85 @@ IP_ADAPTER_ADDRESSES* get_windows_adapters()
   return NULL;
 }
 
-void copy_ipv4_info(IP_ADAPTER_UNICAST_ADDRESS* pip, IP_ADAPTER_GATEWAY_ADDRESS* pgate, LwpaNetintInfo* info)
+void copy_ipv4_info(IP_ADAPTER_UNICAST_ADDRESS* pip, LwpaNetintInfo* info)
 {
   struct sockaddr_in* sin = (struct sockaddr_in*)pip->Address.lpSockaddr;
-  struct sockaddr_in* gw_sin = pgate ? (struct sockaddr_in*)pgate->Address.lpSockaddr : NULL;
-  ULONG mask = 0;
 
-  ConvertLengthToIpv4Mask(pip->OnLinkPrefixLength, &mask);
   LWPA_IP_SET_V4_ADDRESS(&info->addr, ntohl(sin->sin_addr.s_addr));
-  LWPA_IP_SET_V4_ADDRESS(&info->mask, ntohl((uint32_t)mask));
-  if (pgate)
-    LWPA_IP_SET_V4_ADDRESS(&info->gate, ntohl(gw_sin->sin_addr.s_addr));
-  else
-    LWPA_IP_SET_V4_ADDRESS(&info->gate, 0u);
+  info->mask = lwpa_ip_mask_from_length(kLwpaIpTypeV4, pip->OnLinkPrefixLength);
+}
+
+void copy_ipv6_info(IP_ADAPTER_UNICAST_ADDRESS *pip, LwpaNetintInfo *info)
+{
+  struct sockaddr_in6* sin6 = (struct sockaddr_in6*)pip->Address.lpSockaddr;
+
+  LWPA_IP_SET_V6_ADDRESS(&info->addr, sin6->sin6_addr.s6_addr);
+  info->mask = lwpa_ip_mask_from_length(kLwpaIpTypeV6, pip->OnLinkPrefixLength);
 }
 
 void copy_all_netint_info(IP_ADAPTER_ADDRESSES* adapters)
 {
   IP_ADAPTER_ADDRESSES* pcur = adapters;
-  DWORD def_ifindex;
-  bool have_def_index = false;
-  if (NO_ERROR == GetBestInterface(0, &def_ifindex))
-    have_def_index = true;
+
+  // Get the index of the default interface for IPv4
+  DWORD def_ifindex_v4;
+  bool have_def_index_v4 = false;
+  struct sockaddr_in v4_dest;
+  memset(&v4_dest, 0, sizeof v4_dest);
+  v4_dest.sin_family = AF_INET;
+  if (NO_ERROR == GetBestInterfaceEx((struct sockaddr *)&v4_dest, &def_ifindex_v4))
+    have_def_index_v4 = true;
+
+  // And the same for IPv6
+  DWORD def_ifindex_v6;
+  bool have_def_index_v6 = false;
+  struct sockaddr_in6 v6_dest;
+  memset(&v6_dest, 0, sizeof v6_dest);
+  v6_dest.sin6_family = AF_INET6;
+  if (NO_ERROR == GetBestInterfaceEx((struct sockaddr *)&v6_dest, &def_ifindex_v6))
+    have_def_index_v6 = true;
+
   size_t netint_index = 0;
 
   while (pcur)
   {
     // If this is multihomed, there may be multiple addresses under the same adapter
     IP_ADAPTER_UNICAST_ADDRESS* pip = pcur->FirstUnicastAddress;
-    IP_ADAPTER_GATEWAY_ADDRESS* pgate = pcur->FirstGatewayAddress;
     while (pip)
     {
       LwpaNetintInfo* info = &state.lwpa_netints[netint_index];
       switch (pip->Address.lpSockaddr->sa_family)
       {
         case AF_INET:
-          copy_ipv4_info(pip, pgate, info);
+          copy_ipv4_info(pip, info);
+          if (have_def_index_v4 && pcur->IfIndex == def_ifindex_v4)
+          {
+            info->is_default = true;
+            have_def_index_v4 = false;
+          }
+          else
+          {
+            info->is_default = false;
+          }
           break;
         case AF_INET6:
+          copy_ipv6_info(pip, info);
+          if (have_def_index_v6 && pcur->IfIndex == def_ifindex_v6)
+          {
+            info->is_default = true;
+            have_def_index_v6 = false;
+          }
+          else
+          {
+            info->is_default = false;
+          }
+          break;
         default:
-          /* TODO IPv6 */
           pip = pip->Next;
-          if (pgate && pgate->Next)
-            pgate = pgate->Next;
           continue;
       }
 
       info->ifindex = pcur->IfIndex;
-      if (have_def_index && pcur->IfIndex == def_ifindex)
-      {
-        info->is_default = true;
-        state.have_default_netint_index = true;
-        state.default_netint_index = netint_index;
-        have_def_index = false;
-      }
-      else
-      {
-        info->is_default = false;
-      }
       strncpy_s(info->name, LWPA_NETINTINFO_NAME_LEN, pcur->AdapterName, _TRUNCATE);
       if (pcur->PhysicalAddressLength == LWPA_NETINTINFO_MAC_LEN)
         memcpy(info->mac, pcur->PhysicalAddress, LWPA_NETINTINFO_MAC_LEN);
@@ -280,9 +347,6 @@ void copy_all_netint_info(IP_ADAPTER_ADDRESSES* adapters)
       ++netint_index;
 
       pip = pip->Next;
-      // Just in case there's only ever one gateway but more addrs (probably not possible)
-      if (pgate && pgate->Next)
-        pgate = pgate->Next;
     }
     pcur = pcur->Next;
   }
