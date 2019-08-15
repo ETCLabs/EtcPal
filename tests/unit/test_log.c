6c3888176fa372c63569511a3f66bb5b692791ce
Sam Kearney <samuelmkearney@gmail.com>
2019-08-15 08:42:23 -0500

    Making the log test better

--- tests/unit/test_log.c
+++ tests/unit/test_log.c
@@ -172,32 +172,148 @@ TEST(lwpa_log, validate_log_params_works)
   TEST_ASSERT(lwpa_validate_log_params(&lparams));
 }
 
-// Test logging of:
-//    - int values
-//    - time from callback
-//    - weird and missing value in syslog header
-TEST(lwpa_log, logging_int_values)
+// Make sure the "action" member in the LwpaLogParams struct works as expected.
+TEST(lwpa_log, log_action_is_honored)
 {
   LwpaLogParams lparams;
+  lparams.action = kLwpaLogCreateSyslog;
+
+  lparams.log_fn = log_callback;
+  lparams.log_mask = LWPA_LOG_UPTO(LWPA_LOG_DEBUG);
+  strcpy(lparams.syslog_params.hostname, "10.101.17.38");
+  strcpy(lparams.syslog_params.app_name, "My_App");
+  strcpy(lparams.syslog_params.procid, "\0");
+  lparams.syslog_params.facility = LWPA_LOG_KERN;
+  lparams.time_fn = NULL;
+  lparams.context = NULL;
+
+#define LOG_ACTION_TEST_MESSAGE "Test Message"
+
+  const char* expect_syslog_str = "<0>1 - 10.101.17.38 My_App - - - " LOG_ACTION_TEST_MESSAGE;
+  const char* expect_human_str = LOG_ACTION_TEST_MESSAGE;
+  const char* expect_raw_str = LOG_ACTION_TEST_MESSAGE;
+
+  // Try logging only syslog
+  lwpa_log(&lparams, LWPA_LOG_EMERG, "Test Message");
+  // Make sure the callback was called with the syslog and raw strings, but not the human-readable
+  // string.
+  TEST_ASSERT_EQUAL_UINT(log_callback_fake.call_count, 1);
+  TEST_ASSERT(last_log_strings_received.syslog);
+  TEST_ASSERT_UNLESS(last_log_strings_received.human_readable);
+  TEST_ASSERT(last_log_strings_received.raw);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.syslog, expect_syslog_str);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.raw, expect_raw_str);
+
+  // Try logging both
+  lparams.action = kLwpaLogCreateBoth;
+  lwpa_log(&lparams, LWPA_LOG_EMERG, LOG_ACTION_TEST_MESSAGE);
+  // Make sure the callback was called with all three strings.
+  TEST_ASSERT_EQUAL_UINT(log_callback_fake.call_count, 2);
+  TEST_ASSERT(last_log_strings_received.syslog);
+  TEST_ASSERT(last_log_strings_received.human_readable);
+  TEST_ASSERT(last_log_strings_received.raw);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.syslog, expect_syslog_str);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.human_readable, expect_human_str);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.raw, expect_raw_str);
+
+  // Try logging only human-readable
+  lparams.action = kLwpaLogCreateHumanReadableLog;
+  lwpa_log(&lparams, LWPA_LOG_EMERG, LOG_ACTION_TEST_MESSAGE);
+  // Make sure the callback was called with the human-readable and raw strings, but not the syslog
+  // string.
+  TEST_ASSERT_EQUAL_UINT(log_callback_fake.call_count, 3);
+  TEST_ASSERT_UNLESS(last_log_strings_received.syslog);
+  TEST_ASSERT(last_log_strings_received.human_readable);
+  TEST_ASSERT(last_log_strings_received.raw);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.human_readable, expect_human_str);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.raw, expect_raw_str);
+}
+
+// Test valid, weird, and missing values in the syslog header
+TEST(lwpa_log, syslog_header_is_well_formed)
+{
+  LwpaLogParams lparams;
+  char syslog_buf[LWPA_SYSLOG_STR_MAX_LEN];
+
   // A string with a non-ASCII character: "host\xC8name"
   // Should be sanitized to "host?name"
-  const unsigned char weird_hostname[] = {0x68, 0x6f, 0x73, 0x74, 0xc8, 0x6e, 0x61, 0x6d, 0x65, 0x00};
-  char syslog_buf[LWPA_SYSLOG_STR_MAX_LEN];
-  char human_buf[LWPA_HUMAN_LOG_STR_MAX_LEN];
+  const char weird_hostname[] = {0x68, 0x6f, 0x73, 0x74, 0xc8, 0x6e, 0x61, 0x6d, 0x65, 0x00};
+  // A string with some non-printing and non-ASCII characters: "\x012\x034\xff"
+  const char weird_procid[] = {0x01, 0x32, 0x03, 0x34, 0xff, 0x00};
+
+  memcpy(lparams.syslog_params.hostname, weird_hostname, sizeof weird_hostname);
+  memcpy(lparams.syslog_params.procid, weird_procid, sizeof weird_procid);
 
   lparams.action = kLwpaLogCreateSyslog;
   lparams.log_fn = log_callback;
-  memcpy(lparams.syslog_params.hostname, weird_hostname, sizeof weird_hostname);
   strcpy(lparams.syslog_params.app_name, "My_App");
-  lparams.syslog_params.procid[0] = '\0';
   lparams.syslog_params.facility = LWPA_LOG_KERN;
-  lparams.log_mask = 0;
+  lparams.log_mask = LWPA_LOG_UPTO(LWPA_LOG_DEBUG);
   lparams.time_fn = time_callback;
   lparams.context = NULL;
 
+#define SYSLOG_HEADER_TEST_MESSAGE "Test Message"
+
   const char* expect_syslog_str =
-      "<0>1 1970-01-01T00:00:00.000Z host?name My_App - - - Here are some int values: 1 42 4294967295";
-  const char* expect_human_str = "1970-01-01 00:00:00.000Z Here are some int values: 1 42 4294967295";
+      "<0>1 1970-01-01T00:00:00.000Z host?name My_App _2_4? - - " SYSLOG_HEADER_TEST_MESSAGE;
+
+  TEST_ASSERT(lwpa_validate_log_params(&lparams));
+
+  lwpa_log(&lparams, LWPA_LOG_EMERG, LOG_ACTION_TEST_MESSAGE);
+  TEST_ASSERT_EQUAL_UINT(log_callback_fake.call_count, 1);
+  TEST_ASSERT_EQUAL_STRING(last_log_strings_received.syslog, expect_syslog_str);
+
+  // TODO expand...
+}
+
+TEST(lwpa_log, syslog_prival_is_correct)
+{
+  // TODO
+}
+
+// Make sure the log mask member in the LwpaLogParams struct is honored properly.
+TEST(lwpa_log, log_mask_is_honored)
+{
+  LwpaLogParams lparams;
+
+  lparams.action = kLwpaLogCreateSyslog;
+  lparams.log_fn = log_callback;
+  memset(&lparams.syslog_params, 0, sizeof(LwpaSyslogParams));
+  lparams.log_mask = 0;
+  lparams.time_fn = NULL;
+  lparams.context = NULL;
+
+#define LOG_MASK_TEST_MESSAGE "Test Message"
+  // Try logging with the log mask set to 0, should not work.
+  TEST_ASSERT_UNLESS(LWPA_CAN_LOG(&lparams, LWPA_LOG_EMERG));
+  lwpa_log(&lparams, LWPA_LOG_EMERG, LOG_MASK_TEST_MESSAGE);
+  TEST_ASSERT_EQUAL_UINT(log_callback_fake.call_count, 0);
+
+  // Try logging with the log mask including LWPA_LOG_EMERG, should work.
+  lparams.log_mask = LWPA_LOG_UPTO(LWPA_LOG_EMERG);
+  lwpa_log(&lparams, LWPA_LOG_EMERG, LOG_MASK_TEST_MESSAGE);
+  TEST_ASSERT_EQUAL_UINT(log_callback_fake.call_count, 1);
+}
+
+// Make sure the time header is properly present (or absent) as necessary
+TEST(lwpa_log, time_header_is_well_formed)
+{
+}
+
+/*
+// Test logging of int values in the format string.
+TEST(lwpa_log, formatting_int_values)
+{
+  LwpaLogParams lparams;
+  char human_buf[LWPA_HUMAN_LOG_STR_MAX_LEN];
+  char syslog_buf[LWPA_SYSLOG_STR_MAX_LEN];
+
+  lparams.action = kLwpaLogCreateBoth;
+  lparams.log_fn = log_callback;
+  lparams.log_mask = LWPA_LOG_UPTO(LWPA_LOG_EMERG);
+  lparams.time_fn = time_callback;
+  lparams.context = NULL;
+
   const char* expect_raw_str = "Here are some int values: 1 42 4294967295";
 
   TEST_ASSERT(lwpa_validate_log_params(&lparams));
@@ -205,20 +321,15 @@ TEST(lwpa_log, logging_int_values)
 #define INTVAL_FORMAT_STR_AND_ARGS "Here are some int values: %d %d %u", 1, 42, 0xffffffffu
 
   // Try the functions that simply build the log strings
+  // We just check to make sure the
   TEST_ASSERT(lwpa_create_syslog_str(syslog_buf, LWPA_SYSLOG_STR_MAX_LEN, &cur_time, &lparams.syslog_params,
                                      LWPA_LOG_EMERG, INTVAL_FORMAT_STR_AND_ARGS));
-  TEST_ASSERT_EQUAL_STRING(syslog_buf, expect_syslog_str);
+  TEST_ASSERT(strstr(syslog_buf, expect_raw_str));
 
   TEST_ASSERT(lwpa_create_human_log_str(human_buf, LWPA_HUMAN_LOG_STR_MAX_LEN, &cur_time, INTVAL_FORMAT_STR_AND_ARGS));
-  TEST_ASSERT_EQUAL_STRING(human_buf, expect_human_str);
-
-  // Try logging with the log mask set to 0, should not work.
-  TEST_ASSERT_UNLESS(LWPA_CAN_LOG(&lparams, LWPA_LOG_EMERG));
-  lwpa_log(&lparams, LWPA_LOG_EMERG, INTVAL_FORMAT_STR_AND_ARGS);
-  TEST_ASSERT_EQUAL_UINT(log_callback_fake.call_count, 0);
+  TEST_ASSERT(strstr(human_buf, expect_raw_str));
 
   // Try logging only syslog
-  lparams.log_mask = LWPA_LOG_UPTO(LWPA_LOG_EMERG);
   lwpa_log(&lparams, LWPA_LOG_EMERG, INTVAL_FORMAT_STR_AND_ARGS);
   // Make sure the callback was called with the syslog and raw strings, but not the human-readable
   // string.
@@ -254,40 +365,6 @@ TEST(lwpa_log, logging_int_values)
   TEST_ASSERT_EQUAL_STRING(last_log_strings_received.raw, expect_raw_str);
 }
 
-/*
-// Used by the following tests. Try using lwpa_vlog() to log various combinations of syslog and
-// human-readable logging.
-void LogTest::TestLwpaVlogHelper(std::string expect_syslog_str, std::string expect_human_str,
-                                 std::string expect_raw_str, LwpaLogParams* lparams, int pri, const char* format, ...)
-{
-  va_list args;
-  va_start(args, format);
-
-  // Try logging only syslog
-  lparams->action = kLwpaLogCreateSyslog;
-  EXPECT_CALL(*this, VerifyLogCallback(expect_syslog_str, std::string(), expect_raw_str));
-  lwpa_vlog(lparams, pri, format, args);
-  Mock::VerifyAndClearExpectations(this);
-
-  va_end(args);
-  va_start(args, format);
-
-  // Try logging both
-  lparams->action = kLwpaLogCreateBoth;
-  EXPECT_CALL(*this, VerifyLogCallback(expect_syslog_str, expect_human_str, expect_raw_str));
-  lwpa_vlog(lparams, pri, format, args);
-  Mock::VerifyAndClearExpectations(this);
-
-  va_end(args);
-  va_start(args, format);
-
-  // Try logging only human-readable
-  lparams->action = kLwpaLogCreateHumanReadableLog;
-  EXPECT_CALL(*this, VerifyLogCallback(std::string(), expect_human_str, expect_raw_str));
-  lwpa_vlog(lparams, pri, format, args);
-  Mock::VerifyAndClearExpectations(this);
-}
-
 // Test logging of:
 //    - string values
 //    - no time
@@ -295,24 +372,19 @@ void LogTest::TestLwpaVlogHelper(std::string expect_syslog_str, std::string expe
 TEST_F(LogTest, log_strval)
 {
   LwpaLogParams lparams;
-  // A string with some non-printing and non-ASCII characters: "\x012\x034\xff"
-  const unsigned char weird_procid[] = {0x01, 0x32, 0x03, 0x34, 0xff, 0x00};
   char syslog_buf[LWPA_SYSLOG_STR_MAX_LEN];
   char human_buf[LWPA_HUMAN_LOG_STR_MAX_LEN];
 
   lparams.action = kLwpaLogCreateSyslog;
-  lparams.log_fn = log_cb;
-  strcpy(lparams.syslog_params.hostname, "10.101.17.38");
-  memcpy(lparams.syslog_params.procid, weird_procid, sizeof weird_procid);
-  lparams.syslog_params.app_name[0] = '\0';
-  lparams.syslog_params.facility = LWPA_LOG_LOCAL2;
+  lparams.log_fn = log_callback;
+  memset(&lparams.syslog_params, 0, sizeof(LwpaSyslogParams));
   lparams.log_mask = 0;
-  lparams.time_fn = nullptr;
-  lparams.context = this;
+  lparams.time_fn = NULL;
+  lparams.context = NULL;
 
-  std::string expect_syslog_str = "<149>1 - 10.101.17.38 - _2_4? - - Here are some string values: hey wassup hello";
-  std::string expect_human_str = "Here are some string values: hey wassup hello";
-  std::string expect_raw_str = expect_human_str;
+  const char* expect_syslog_str = "<149>1 - 10.101.17.38 - _2_4? - - Here are some string values: hey wassup hello";
+  const char* expect_human_str = "Here are some string values: hey wassup hello";
+  const char* expect_raw_str = expect_human_str;
 
   TEST_ASSERT(lwpa_validate_log_params(&lparams));
 
@@ -437,7 +509,11 @@ TEST_GROUP_RUNNER(lwpa_log)
   RUN_TEST_CASE(lwpa_log, log_mask_macros_work);
   RUN_TEST_CASE(lwpa_log, sanitize_syslog_params_works);
   RUN_TEST_CASE(lwpa_log, validate_log_params_works);
-  RUN_TEST_CASE(lwpa_log, logging_int_values);
+  RUN_TEST_CASE(lwpa_log, log_action_is_honored);
+  RUN_TEST_CASE(lwpa_log, syslog_header_is_well_formed);
+  RUN_TEST_CASE(lwpa_log, syslog_prival_is_correct);
+  RUN_TEST_CASE(lwpa_log, log_mask_is_honored);
+  RUN_TEST_CASE(lwpa_log, time_header_is_well_formed);
 }
 
 void run_all_tests(void)
