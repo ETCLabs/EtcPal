2a277fb7076e760f6c0ba6cb6b44fbfe891a0b2b
Sam Kearney <samuelmkearney@gmail.com>
2019-09-09 16:30:58 -0500

    Update version files for EtcPal build 0.3.0.3

--- include/etcpal/version.h
+++ include/etcpal/version.h
@@ -32,14 +32,14 @@
 #define ETCPAL_VERSION_MAJOR 0
 #define ETCPAL_VERSION_MINOR 3
 #define ETCPAL_VERSION_PATCH 0
-#define ETCPAL_VERSION_BUILD 2
+#define ETCPAL_VERSION_BUILD 3
 /*! @} */
 
 /*! \name EtcPal version strings
  *  @{
  */
-#define ETCPAL_VERSION_STRING "0.3.0.2"
-#define ETCPAL_VERSION_DATESTR "04.Sep.2019"
+#define ETCPAL_VERSION_STRING "0.3.0.3"
+#define ETCPAL_VERSION_DATESTR "09.Sep.2019"
 #define ETCPAL_VERSION_COPYRIGHT "Copyright 2019 ETC Inc."
 #define ETCPAL_VERSION_PRODUCTNAME "EtcPal"
 /*! @} */
