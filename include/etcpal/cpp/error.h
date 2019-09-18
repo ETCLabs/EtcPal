19ff892db17ad2925b1dcf0acc5d930f3dbd02d3
Sam Kearney <samuelmkearney@gmail.com>
2019-09-18 07:12:50 -0600

    Add etcpal::Result class

---
+++ include/etcpal/cpp/error.h
@@ -0,0 +1,171 @@
+/******************************************************************************
+ * Copyright 2019 ETC Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************
+ * This file is a part of EtcPal. For more information, go to:
+ * https://github.com/ETCLabs/EtcPal
+ ******************************************************************************/
+/// \file etcpal/cpp/error.h
+/// \brief C++ wrapper and utilities for etcpal/error.h
+#ifndef ETCPAL_CPP_ERROR_H
+#define ETCPAL_CPP_ERROR_H
+
+#include <string>
+#include "etcpal/error.h"
+
+namespace etcpal
+{
+/// \defgroup etcpal_cpp_error etcpal_cpp_error
+/// \ingroup etcpal_cpp
+/// \brief C++ utilities for the \ref etcpal_error module.
+
+/// \brief A wrapper class for the EtcPal error type.
+/// \ingroup etcpal_cpp_error
+///
+/// \details
+/// Provides C++ syntactic sugar on top of #etcpal_error_t codes. A Result can be created from an
+/// #etcpal_error_t, and contains the code and a string representation. If the Result contains
+/// #kEtcPalErrOk, it will evaluate to true in expressions (and IsOk() will return true).
+/// Otherwise, the Result evaluates to false.
+///
+/// The human-readable string associated with the Result can be retrieved with ToString() and
+/// ToCString(); this will be the same string available from etcpal_strerror() for the given error
+/// code.
+///
+/// Example with an "OK" error code:
+/// \code
+/// auto result = etcpal::Result(kEtcPalErrOk); // or etcpal::Result::Ok()
+/// if (result)
+/// {
+///    // This block will be entered...
+/// }
+/// \endcode
+///
+/// Example with a non-"OK" error code:
+/// \code
+/// auto result = etcpal::Result(kEtcPalErrNoMem);
+/// if (result)
+/// {
+///   // This block will not be entered
+/// }
+/// else
+/// {
+///   // This block will be entered
+///   printf("Error: '%s'", result.ToCString());
+///   // Or
+///   std::cout << "Error: '" << result.ToString() << "'\n";
+///   // Prints "Error: 'Out of memory'"
+/// }
+/// \endcode
+class Result
+{
+public:
+  Result() = delete;
+  Result(etcpal_error_t code);
+  Result& operator=(etcpal_error_t code);
+
+  bool IsOk() const;
+  etcpal_error_t code() const;
+  std::string ToString() const;
+  const char* ToCString() const;
+
+  explicit operator bool() const;
+
+  static Result Ok();
+
+private:
+  etcpal_error_t code_{kEtcPalErrOk};
+  const char* str_{nullptr};
+
+  void SetCode(etcpal_error_t code);
+};
+
+/// \brief Construct a Result from an error code.
+inline Result::Result(etcpal_error_t code)
+{
+  SetCode(code);
+}
+
+/// \brief Assign an error code to this Result.
+inline Result& Result::operator=(etcpal_error_t code)
+{
+  SetCode(code);
+  return *this;
+}
+
+/// \brief Whether this Result contains the code #kEtcPalErrOk.
+/// \details #kEtcPalErrOk is the only #etcpal_error_t code that does not indicate an error.
+/// \returns Whether the result is OK (not an error).
+inline bool Result::IsOk() const
+{
+  return code_ == kEtcPalErrOk;
+}
+
+/// \brief Get the underlying code from a Result.
+inline etcpal_error_t Result::code() const
+{
+  return code_;
+}
+
+/// \brief Get a descriptive string for this Result as a std::string.
+inline std::string Result::ToString() const
+{
+  return str_;
+}
+
+/// \brief Get a descriptive string for this Result as a C string.
+inline const char* Result::ToCString() const
+{
+  return str_;
+}
+
+/// \brief Evaluate the Result inline - evaluates true if the result is #kEtcPalErrOk, false
+///        otherwise.
+inline Result::operator bool() const
+{
+  return IsOk();
+}
+
+/// \brief Construct a result containing #kEtcPalErrOk.
+inline Result Result::Ok()
+{
+  return Result(kEtcPalErrOk);
+}
+
+// Internal function to set the internal code and string for a given code.
+inline void Result::SetCode(etcpal_error_t code)
+{
+  code_ = code;
+  str_ = etcpal_strerror(code);
+}
+
+// equality operators
+inline bool operator==(etcpal_error_t code, const Result& result)
+{
+  return code == result.code();
+}
+
+inline bool operator==(const Result& result, etcpal_error_t code)
+{
+  return result.code() == code;
+}
+
+inline bool operator==(const Result& result_a, const Result& result_b)
+{
+  return result_a.code() == result_b.code();
+}
+
+};  // namespace etcpal
+
+#endif  // ETCPAL_CPP_ERROR_H
---
+++ tests/unit/cpp/test_error.cpp
@@ -0,0 +1,112 @@
+/******************************************************************************
+ * Copyright 2019 ETC Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************
+ * This file is a part of EtcPal. For more information, go to:
+ * https://github.com/ETCLabs/EtcPal
+ ******************************************************************************/
+#include "etcpal/cpp/error.h"
+#include "etcpal/common.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(etcpal_cpp_error);
+
+TEST_SETUP(etcpal_cpp_error)
+{
+}
+
+TEST_TEAR_DOWN(etcpal_cpp_error)
+{
+}
+
+TEST(etcpal_cpp_error, constructors_function_correctly)
+{
+  // Explicit constructor from "OK" error code
+  etcpal::Result exp_ok(kEtcPalErrOk);
+  TEST_ASSERT_TRUE(exp_ok);
+  TEST_ASSERT_TRUE(exp_ok.IsOk());
+  TEST_ASSERT_EQUAL(exp_ok.code(), kEtcPalErrOk);
+  TEST_ASSERT_NOT_NULL(exp_ok.ToCString());
+  TEST_ASSERT_FALSE(exp_ok.ToString().empty());
+
+  // Explicit constructor from non-"OK" error code
+  etcpal::Result exp_not_ok(kEtcPalErrSys);
+  TEST_ASSERT_FALSE(exp_not_ok);
+  TEST_ASSERT_FALSE(exp_not_ok.IsOk());
+  TEST_ASSERT_EQUAL(exp_not_ok.code(), kEtcPalErrSys);
+  TEST_ASSERT_NOT_NULL(exp_not_ok.ToCString());
+  TEST_ASSERT_FALSE(exp_not_ok.ToString().empty());
+}
+
+TEST(etcpal_cpp_error, static_ok_works)
+{
+  auto result = etcpal::Result::Ok();
+  TEST_ASSERT_TRUE(result);
+  TEST_ASSERT_EQUAL(result.code(), kEtcPalErrOk);
+
+  etcpal::Result result_2(kEtcPalErrOk);
+  TEST_ASSERT(result_2 == result);
+}
+
+TEST(etcpal_cpp_error, assignment_operators)
+{
+  etcpal::Result result = kEtcPalErrOk;
+  TEST_ASSERT_TRUE(result);
+  TEST_ASSERT_EQUAL(result.code(), kEtcPalErrOk);
+
+  result = kEtcPalErrSys;
+  TEST_ASSERT_FALSE(result);
+  TEST_ASSERT_EQUAL(result.code(), kEtcPalErrSys);
+
+  auto result_2 = result;
+  TEST_ASSERT_FALSE(result_2);
+  TEST_ASSERT_EQUAL(result.code(), result_2.code());
+  TEST_ASSERT_EQUAL_STRING(result.ToCString(), result_2.ToCString());
+  TEST_ASSERT(result.ToString() == result_2.ToString());
+}
+
+TEST(etcpal_cpp_error, equality_operators)
+{
+  etcpal::Result result(kEtcPalErrOk);
+  TEST_ASSERT(result == kEtcPalErrOk);
+  TEST_ASSERT(kEtcPalErrOk == result);
+
+  etcpal::Result result_2(kEtcPalErrOk);
+  TEST_ASSERT(result_2 == result);
+  TEST_ASSERT(result == result_2);
+
+  etcpal::Result result_3(kEtcPalErrSys);
+  TEST_ASSERT_UNLESS(result_3 == result);
+  TEST_ASSERT_UNLESS(result == result_3);
+}
+
+TEST(etcpal_cpp_error, strings_exist_for_all_codes)
+{
+  for (int i = 0; i > 0 - ETCPAL_NUM_ERROR_CODES; --i)
+  {
+    etcpal::Result result(static_cast<etcpal_error_t>(i));
+    TEST_ASSERT_UNLESS_MESSAGE(result.ToString().empty(),
+                               std::string("Failed on iteration " + std::to_string(i)).c_str());
+    TEST_ASSERT_NOT_NULL_MESSAGE(result.ToCString(), std::string("Failed on iteration " + std::to_string(i)).c_str());
+  }
+}
+
+TEST_GROUP_RUNNER(etcpal_cpp_error)
+{
+  RUN_TEST_CASE(etcpal_cpp_error, constructors_function_correctly);
+  RUN_TEST_CASE(etcpal_cpp_error, static_ok_works);
+  RUN_TEST_CASE(etcpal_cpp_error, assignment_operators);
+  RUN_TEST_CASE(etcpal_cpp_error, equality_operators);
+  RUN_TEST_CASE(etcpal_cpp_error, strings_exist_for_all_codes);
+}
