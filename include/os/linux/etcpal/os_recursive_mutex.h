36efa4f4cb363b4b98afac96c2297caba50f9e2f
Sam Kearney <samuelmkearney@gmail.com>
2020-11-03 15:24:43 -0600

    Recursive mutex implementations: macOS and Linux

---
+++ include/os/linux/etcpal/os_recursive_mutex.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright 2020 ETC Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************
+ * This file is a part of EtcPal. For more information, go to:
+ * https://github.com/ETCLabs/EtcPal
+ ******************************************************************************/
+
+#ifndef ETCPAL_OS_RECURSIVE_MUTEX_H_
+#define ETCPAL_OS_RECURSIVE_MUTEX_H_
+
+#include <stdbool.h>
+#include <pthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef pthread_mutex_t etcpal_recursive_mutex_t;
+
+#define ETCPAL_RECURSIVE_MUTEX_HAS_TIMED_LOCK 0
+
+bool etcpal_recursive_mutex_create(etcpal_recursive_mutex_t* id);
+#define etcpal_recursive_mutex_lock(idptr) ((bool)(!pthread_mutex_lock(idptr)))
+#define etcpal_recursive_mutex_try_lock(idptr) ((bool)(!pthread_mutex_trylock(idptr)))
+bool etcpal_recursive_mutex_timed_lock(etcpal_recursive_mutex_t* id, int timeout_ms);
+#define etcpal_recursive_mutex_unlock(idptr) ((void)pthread_mutex_unlock(idptr))
+#define etcpal_recursive_mutex_destroy(idptr) ((void)pthread_mutex_destroy(idptr))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ETCPAL_OS_RECURSIVE_MUTEX_H_ */
---
+++ src/os/macos/etcpal/os_recursive_mutex.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * Copyright 2020 ETC Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************
+ * This file is a part of EtcPal. For more information, go to:
+ * https://github.com/ETCLabs/EtcPal
+ ******************************************************************************/
+
+#include "etcpal/recursive_mutex.h"
+
+bool etcpal_recursive_mutex_create(etcpal_recursive_mutex_t* id)
+{
+  if (id)
+  {
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    return (bool)(!pthread_mutex_init(id, &attr));
+  }
+  return false;
+}
+
+bool etcpal_recursive_mutex_timed_lock(etcpal_recursive_mutex_t* id, int timeout_ms)
+{
+  return (timeout_ms == 0 ? etcpal_recursive_mutex_try_lock(id) : etcpal_recursive_mutex_lock(id));
+}
