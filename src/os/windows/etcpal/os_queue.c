a3c51737879cdf2b459dabeba09ed4350a032bb0
Noah Meltzer <noah.meltzer@etcconnect.com>
2021-03-30 12:26:21 -0500

    Initial files for windows queue

---
+++ src/os/windows/etcpal/os_queue.c
@@ -0,0 +1,10 @@
+#include "..\..\..\..\include\os\windows\etcpal\os_queue.h"
+
+bool etcpal_queue_create(etcpal_queue_t* id, size_t size, std::size_t item_size)
+{
+  return false;
+}
+
+void etcpal_queue_destroy(etcpal_queue_t* id)
+{
+}
