84385e5d5dfcbff6320f46f8d540b663ae01232e
ETC CI <noreply.ci@etcconnect.com>
2022-03-24 17:53:48 +0000

    [skip ci] Update version files for build 0.5.0.4

--- include/etcpal/version.h
+++ include/etcpal/version.h
@@ -34,7 +34,7 @@
 #define ETCPAL_VERSION_MAJOR 0
 #define ETCPAL_VERSION_MINOR 5
 #define ETCPAL_VERSION_PATCH 0
-#define ETCPAL_VERSION_BUILD 3
+#define ETCPAL_VERSION_BUILD 4
 /**
  * @}
  */
@@ -43,8 +43,8 @@
  * @name EtcPal version strings
  * @{
  */
-#define ETCPAL_VERSION_STRING "0.5.0.3"
-#define ETCPAL_VERSION_DATESTR "21.Mar.2022"
+#define ETCPAL_VERSION_STRING "0.5.0.4"
+#define ETCPAL_VERSION_DATESTR "24.Mar.2022"
 #define ETCPAL_VERSION_COPYRIGHT "Copyright 2022 ETC Inc."
 #define ETCPAL_VERSION_PRODUCTNAME "EtcPal"
 /**
