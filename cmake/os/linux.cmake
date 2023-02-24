5ce528d50da32f5cb2ace41ed6a85aa178856cb0
Christian Reese <christian.reese@etcconnect.com>
2023-02-24 14:34:40 -0600

    Add sources & reorganize groupings in VS project browser

--- cmake/os/linux.cmake
+++ cmake/os/linux.cmake
@@ -1,6 +1,6 @@
 # Linux OS compilation support for EtcPal
 
-set(ETCPAL_OS_ADDITIONAL_SOURCES
+set(ETCPAL_OS_ADDITIONAL_HEADERS
   ${ETCPAL_ROOT}/include/os/linux/etcpal/os_event_group.h
   ${ETCPAL_ROOT}/include/os/linux/etcpal/os_mutex.h
   ${ETCPAL_ROOT}/include/os/linux/etcpal/os_queue.h
@@ -9,7 +9,9 @@ set(ETCPAL_OS_ADDITIONAL_SOURCES
   ${ETCPAL_ROOT}/include/os/linux/etcpal/os_sem.h
   ${ETCPAL_ROOT}/include/os/linux/etcpal/os_signal.h
   ${ETCPAL_ROOT}/include/os/linux/etcpal/os_thread.h
+)
 
+set(ETCPAL_OS_ADDITIONAL_SOURCES
   ${ETCPAL_ROOT}/src/os/linux/etcpal/os_error.h
   ${ETCPAL_ROOT}/src/os/linux/etcpal/os_error.c
   ${ETCPAL_ROOT}/src/os/linux/etcpal/os_event_group.c
