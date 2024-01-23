3c1051368be9cbe5f65e59f0fa90e45f2e1e0bf3
Michael Feist <michael.feist@etcconnect.com>
2024-01-23 14:36:44 -0600

    Remove Zephyr as a supported NET_TARGET

--- cmake/etcpal-os.cmake
+++ cmake/etcpal-os.cmake
@@ -15,7 +15,8 @@ set(VALID_ETCPAL_NET_TARGETS
   macos
   mqx
   windows
-  zephyr
+
+  # zephyr
 )
 
 set(ETCPAL_OS_TARGET "" CACHE STRING "OS provider for EtcPal. See tools/cmake/etcpal-os.cmake for valid values.")
