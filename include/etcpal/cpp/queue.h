95b8b949c067b3c75233ca52640b19b3e1db7c1c
MIDDLETON\nmeltzer <noah.meltzer@etcconnect.com>
2024-03-19 09:01:40 -0500

    1. Add throw-away byte to remove warning because we cannot have a 0-size array

--- include/etcpal/cpp/queue.h
+++ include/etcpal/cpp/queue.h
@@ -124,7 +124,8 @@ public:
 
 private:
   etcpal_queue_t queue_{};
-  uint8_t        buffer[N * sizeof(T)];
+  // Cannot have a 0-sized array in the case where N=0
+  uint8_t        buffer[N * sizeof(T) + 1];
 };
 
 /// @brief Create a new queue.
