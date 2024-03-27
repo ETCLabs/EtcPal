bb6c36e779114daf376a076f993e28b974b7db4b
MIDDLETON\nmeltzer <noah.meltzer@etcconnect.com>
2024-03-27 15:38:25 -0500

    1. revert some changes

--- include/os/zephyr/etcpal/os_queue.h
+++ include/os/zephyr/etcpal/os_queue.h
@@ -45,24 +45,24 @@ bool etcpal_queue_create(etcpal_queue_t* id, size_t size, size_t item_size);
 bool etcpal_queue_create_static(etcpal_queue_t* id, size_t size, size_t item_size, uint8_t* buffer);
 void etcpal_queue_destroy(etcpal_queue_t* id);
 
-bool etcpal_queue_send(etcpal_queue_t* id, const void* data);
+#define etcpal_queue_send(idptr, dataptr) (etcpal_queue_timed_send((idptr), (dataptr), ETCPAL_WAIT_FOREVER))
 bool etcpal_queue_timed_send(etcpal_queue_t* id, const void* data, int timeout_ms);
-bool etcpal_queue_send_from_isr(etcpal_queue_t* id, const void* data);
+#define etcpal_queue_send_from_isr etcpal_queue_send
 
-bool etcpal_queue_receive(etcpal_queue_t* id, void* data);
+#define etcpal_queue_receive(idptr, dataptr) (etcpal_queue_timed_receive((idptr), (dataptr), ETCPAL_WAIT_FOREVER))
 bool etcpal_queue_timed_receive(etcpal_queue_t* id, void* data, int timeout_ms);
-bool etcpal_queue_receive_from_isr(etcpal_queue_t* id, void* data);
+#define etcpal_queue_receive_from_isr etcpal_queue_receive
 
 bool etcpal_queue_reset(etcpal_queue_t* id);
 
 bool etcpal_queue_is_empty(const etcpal_queue_t* id);
-bool etcpal_queue_is_empty_from_isr(const etcpal_queue_t* id);
+#define etcpal_queue_is_empty_from_isr etcpal_queue_is_empty
 
 bool etcpal_queue_is_full(const etcpal_queue_t* id);
-bool etcpal_queue_is_full_from_isr(const etcpal_queue_t* id);
+#define etcpal_queue_is_full_from_isr etcpal_queue_is_full
 
 size_t etcpal_queue_slots_used(const etcpal_queue_t* id);
-size_t etcpal_queue_slots_used_from_isr(const etcpal_queue_t* id);
+#define etcpal_queue_slots_used_from_isr etcpal_queue_slots_used
 
 size_t etcpal_queue_slots_available(const etcpal_queue_t* id);
 
