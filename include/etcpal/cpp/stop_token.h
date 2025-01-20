2e616dc36ff9cd59b139fbd4d1073ac989a14a20
Danni Hu <danni.hu@etcconnect.com>
2025-01-20 15:52:42 -0600

    refac(cpp): `StopCallback` safer implementation

--- include/etcpal/cpp/stop_token.h
+++ include/etcpal/cpp/stop_token.h
@@ -26,46 +26,6 @@ class StopToken;
 template <typename Callback, typename Allocator = DefaultAllocator>
 class StopCallback;
 
-namespace detail
-{
-
-class ConstCallbackBase
-{
-public:
-  virtual ~ConstCallbackBase() noexcept = default;
-  virtual void operator()() const       = 0;
-};
-
-class MutableCallbackBase
-{
-public:
-  virtual ~MutableCallbackBase() noexcept = default;
-  virtual void operator()()               = 0;
-};
-
-template <typename Function, typename = void>
-class Callback : public MutableCallbackBase
-{
-public:
-  Callback(Function&& fun) : fun_{std::move(fun)} {}
-  void operator()() override { fun_(); }
-
-private:
-  Function fun_;
-};
-template <typename Function>
-class Callback<Function, void_t<decltype(std::declval<const Function>()())>> : public ConstCallbackBase
-{
-public:
-  Callback(Function&& fun) : fun_{std::move(fun)} {}
-  void operator()() const override { fun_(); }
-
-private:
-  Function fun_;
-};
-
-}  // namespace detail
-
 template <typename Allocator = DefaultAllocator>
 class StopSource
 {
@@ -119,8 +79,19 @@ private:
   std::shared_ptr<StopContext> ctrl_block_ = {};
 };
 
+namespace detail
+{
+
+class StopCallbackBase
+{
+public:
+  virtual void invoke() const = 0;
+};
+
+}  // namespace detail
+
 template <typename Callback, typename Allocator>
-class StopCallback
+class StopCallback : private detail::StopCallbackBase
 {
 public:
   using callback_type = Callback;
@@ -141,8 +112,10 @@ public:
   ~StopCallback() noexcept;
 
 private:
-  StopToken<Allocator>                    token_;
-  mutable detail::Callback<callback_type> callback_;
+  void invoke() const override { callback_(); }
+
+  StopToken<Allocator>  token_;
+  mutable callback_type callback_;
 };
 
 #if (__cplusplus >= 201703L)
@@ -159,102 +132,9 @@ StopCallback(StopToken<Allocator>&&, Callback&&)
 template <typename Allocator>
 struct etcpal::StopSource<Allocator>::StopContext
 {
-  struct CallbackRef
-  {
-    bool is_const;
-    bool has_value = false;
-    union RefUnion
-    {
-      constexpr RefUnion() noexcept : null_callback{} {}
-      constexpr RefUnion(const detail::ConstCallbackBase& init) noexcept : const_callback{init} {}
-      constexpr RefUnion(detail::MutableCallbackBase& init) noexcept : mutable_callback{init} {}
-
-      std::nullptr_t                                          null_callback;
-      std::reference_wrapper<const detail::ConstCallbackBase> const_callback;
-      std::reference_wrapper<detail::MutableCallbackBase>     mutable_callback;
-    } fun;
-
-    constexpr CallbackRef() = default;
-    constexpr CallbackRef(const detail::ConstCallbackBase& f) noexcept : is_const{true}, has_value{true}, fun{f} {}
-    constexpr CallbackRef(detail::MutableCallbackBase& f) noexcept : is_const{false}, has_value{true}, fun{f} {}
-
-    ~CallbackRef() noexcept { deinit(); }
-
-    constexpr auto operator=(std::nullptr_t rhs) noexcept -> CallbackRef&
-    {
-      deinit();
-      has_value = false;
-      new (std::addressof(fun.null_callback)) std::nullptr_t;
-
-      return *this;
-    }
-    constexpr auto operator=(const detail::ConstCallbackBase& rhs) noexcept -> CallbackRef&
-    {
-      deinit();
-      has_value = true;
-      is_const  = true;
-      new (std::addressof(fun.const_callback)) decltype(fun.const_callback){rhs};
-
-      return *this;
-    }
-    constexpr auto operator=(detail::MutableCallbackBase& rhs) noexcept -> CallbackRef&
-    {
-      deinit();
-      has_value = true;
-      is_const  = false;
-      new (std::addressof(fun.mutable_callback)) decltype(fun.mutable_callback){rhs};
-
-      return *this;
-    }
-
-    void operator()() const noexcept
-    {
-      if (!has_value)
-      {
-        return;
-      }
-
-      if (is_const)
-      {
-        fun.const_callback();
-      }
-      else
-      {
-        fun.mutable_callback();
-      }
-    }
-
-    constexpr void deinit() noexcept
-    {
-      if (!has_value)
-      {
-        using T = std::nullptr_t;
-        fun.null_callback.~T();
-      }
-      else if (is_const)
-      {
-        using T = decltype(fun.const_callback);
-        fun.const_callback.~T();
-      }
-      else
-      {
-        using T = decltype(fun.mutable_callback);
-        fun.mutable_callback.~T();
-      }
-    }
-
-    [[nodiscard]] constexpr bool operator==(std::nullptr_t rhs) const noexcept { return !has_value; }
-    [[nodiscard]] constexpr bool operator==(const detail::ConstCallbackBase& rhs) const noexcept
-    {
-      return has_value && is_const && (std::addressof(fun.const_callback.get()) == std::addressof(rhs));
-    }
-    [[nodiscard]] constexpr bool operator==(const detail::MutableCallbackBase& rhs) const noexcept
-    {
-      return has_value && is_const && (std::addressof(fun.mutable_callback.get()) == std::addressof(rhs));
-    }
-  };
-  using CallbackAllocator = typename std::allocator_traits<Allocator>::template rebind_alloc<CallbackRef>;
-  using CallbackStorage   = std::vector<CallbackRef, CallbackAllocator>;
+  using CallbackAllocator =
+      typename std::allocator_traits<Allocator>::template rebind_alloc<const detail::StopCallbackBase*>;
+  using CallbackStorage = std::vector<const detail::StopCallbackBase*, CallbackAllocator>;
 
   StopContext() = default;
   StopContext(const CallbackAllocator& alloc) : callbacks{alloc} {}
@@ -311,20 +191,11 @@ bool etcpal::StopSource<Allocator>::request_stop() noexcept
     return false;
   }
 
-  for (const auto& var : *ctrl_block_->callbacks.lock())
+  for (const auto* const var : *ctrl_block_->callbacks.lock())
   {
-    if (!var.has_value)
+    if (var)
     {
-      continue;
-    }
-
-    if (var.is_const)
-    {
-      var.fun.const_callback();
-    }
-    else
-    {
-      var.fun.mutable_callback();
+      var->invoke();
     }
   }
 
@@ -359,16 +230,16 @@ etcpal::StopCallback<Callback, Allocator>::StopCallback(const StopToken<Allocato
   }
 
   const auto callbacks = token_.ctrl_block_->callbacks.lock();
-  for (auto& fun : *callbacks)
+  for (const auto* fun : *callbacks)
   {
-    if (!fun.has_value)
+    if (!fun)
     {
-      fun = callback_;
+      fun = this;
       return;
     }
   }
 
-  callbacks->push_back(callback_);
+  callbacks->push_back(this);
 }
 
 template <typename Callback, typename Allocator>
@@ -388,7 +259,8 @@ etcpal::StopCallback<Callback, Allocator>::~StopCallback() noexcept
   }
 
   const auto callbacks = token_.ctrl_block_->callbacks.lock();
-  const auto position  = std::find(std::begin(*callbacks), std::end(*callbacks), callback_);
+  const auto position =
+      std::find(std::begin(*callbacks), std::end(*callbacks), static_cast<detail::StopCallbackBase*>(this));
   if (position == std::end(*callbacks))
   {
     return;
