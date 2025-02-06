fec943190958ce189518a1e5b08d56aa189aa435
Danni Hu <danni.hu@etcconnect.com>
2025-02-06 08:59:58 -0600

    fix(test): don't try to link stop token tests without OS

--- tests/unit/cpp/test_main.cpp
+++ tests/unit/cpp/test_main.cpp
@@ -21,7 +21,6 @@
 
 extern "C" void run_all_tests(void)  // NOLINT
 {
-  RUN_TEST_GROUP(etcpal_cpp_stop_token);
   RUN_TEST_GROUP(etcpal_cpp_error);
   RUN_TEST_GROUP(etcpal_cpp_hash);
   RUN_TEST_GROUP(etcpal_cpp_uuid);
@@ -36,6 +35,7 @@ extern "C" void run_all_tests(void)  // NOLINT
   RUN_TEST_GROUP(etcpal_cpp_log_timestamp);
   RUN_TEST_GROUP(etcpal_cpp_log);
   RUN_TEST_GROUP(etcpal_cpp_mutex);
+  RUN_TEST_GROUP(etcpal_cpp_stop_token);
   RUN_TEST_GROUP(etcpal_cpp_synchronized);
 #if !DISABLE_RECURSIVE_MUTEX_TESTS
   RUN_TEST_GROUP(etcpal_cpp_recursive_mutex);
