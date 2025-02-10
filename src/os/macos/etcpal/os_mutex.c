20da11ee644589eda1fbc1a2f84c5ef05adab373
Danni Hu <danni.hu@etcconnect.com>
2025-02-10 10:22:58 -0600

    fix(os); shorter sleep loop

--- src/os/macos/etcpal/os_mutex.c
+++ src/os/macos/etcpal/os_mutex.c
@@ -20,6 +20,8 @@
 #include <pthread.h>
 #include <time.h>
 
+#include <etcpal/thread.h>
+
 #include "etcpal/mutex.h"
 
 static bool timed_out(const struct timespec* timeout_spec)
@@ -64,7 +66,7 @@ bool etcpal_mutex_timed_lock(etcpal_mutex_t* id, int timeout_ms)
 
     const struct timespec to_sleep = {1, 0};
     struct timespec       remaining;
-    if (0 != nanosleep(&to_sleep, &remaining))
+    if (kEtcPalErrOk != etcpal_thread_sleep(1))
     {
       break;
     }
