ec972c1c0523bf53c6b01fb51df73c39cc1399e9
Danni Hu <danni.hu@etcconnect.com>
2025-02-10 09:26:00 -0600

    feat(os): timed wait for Linux and MacOS mutex, rwlock, event_group

--- src/os/linux/etcpal/os_rwlock.c
+++ src/os/linux/etcpal/os_rwlock.c
@@ -21,10 +21,54 @@
 
 bool etcpal_rwlock_timed_readlock(etcpal_rwlock_t* id, int timeout_ms)
 {
-  return (timeout_ms == 0 ? etcpal_rwlock_try_readlock(id) : etcpal_rwlock_readlock(id));
+  if (timeout_ms == ETCPAL_WAIT_FOREVER)
+  {
+    return etcpal_rwlock_readlock(id);
+  }
+  if (timeout_ms == 0)
+  {
+    return etcpal_rwlock_try_readlock(id);
+  }
+
+  const int             timeout_sec = timeout_ms / 1000;
+  const struct timespec time        = {timeout_sec, 1000000 * (timeout_ms - (timeout_sec * 1000))};
+
+  struct timespec timeout_spec;
+  clock_gettime(CLOCK_REALTIME, &timeout_spec);
+  timeout_spec.tv_sec += time.tv_sec;
+  timeout_spec.tv_nsec += time.tv_nsec;
+  if (timeout_spec.tv_nsec >= 1000000000)
+  {
+    timeout_spec.tv_sec += 1;
+    timeout_spec.tv_nsec -= 1000000000;
+  }
+
+  return 0 == pthread_rwlock_timedrdlock(id, &timeout_spec);
 }
 
 bool etcpal_rwlock_timed_writelock(etcpal_rwlock_t* id, int timeout_ms)
 {
-  return (timeout_ms == 0 ? etcpal_rwlock_try_writelock(id) : etcpal_rwlock_writelock(id));
+  if (timeout_ms == ETCPAL_WAIT_FOREVER)
+  {
+    return etcpal_rwlock_writelock(id);
+  }
+  if (timeout_ms == 0)
+  {
+    return etcpal_rwlock_try_writelock(id);
+  }
+
+  const int             timeout_sec = timeout_ms / 1000;
+  const struct timespec time        = {timeout_sec, 1000000 * (timeout_ms - (timeout_sec * 1000))};
+
+  struct timespec timeout_spec;
+  clock_gettime(CLOCK_REALTIME, &timeout_spec);
+  timeout_spec.tv_sec += time.tv_sec;
+  timeout_spec.tv_nsec += time.tv_nsec;
+  if (timeout_spec.tv_nsec >= 1000000000)
+  {
+    timeout_spec.tv_sec += 1;
+    timeout_spec.tv_nsec -= 1000000000;
+  }
+
+  return 0 == pthread_rwlock_timedwrlock(id, &timeout_spec);
 }
--- src/os/macos/etcpal/os_mutex.c
+++ src/os/macos/etcpal/os_mutex.c
@@ -21,5 +21,27 @@
 
 bool etcpal_mutex_timed_lock(etcpal_mutex_t* id, int timeout_ms)
 {
-  return (timeout_ms == 0 ? etcpal_mutex_try_lock(id) : etcpal_mutex_lock(id));
+  if (timeout_ms == ETCPAL_WAIT_FOREVER)
+  {
+    return etcpal_mutex_lock(id);
+  }
+  if (timeout_ms == 0)
+  {
+    return etcpal_mutex_try_lock(id);
+  }
+
+  const int             timeout_sec = timeout_ms / 1000;
+  const struct timespec time        = {timeout_sec, 1000000 * (timeout_ms - (timeout_sec * 1000))};
+
+  struct timespec timeout_spec;
+  clock_gettime(CLOCK_REALTIME, &timeout_spec);
+  timeout_spec.tv_sec += time.tv_sec;
+  timeout_spec.tv_nsec += time.tv_nsec;
+  if (timeout_spec.tv_nsec >= 1000000000)
+  {
+    timeout_spec.tv_sec += 1;
+    timeout_spec.tv_nsec -= 1000000000;
+  }
+
+  return 0 == pthread_mutex_timedlock(id, &timeout_spec);
 }
