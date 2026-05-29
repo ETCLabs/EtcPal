be230aa169eadcb3291359b9346fe612f9f12fa6
ETC CI <noreply.ci@etcconnect.com>
2026-05-29 16:05:30 +0000

    [skip ci] Update files for official build v1.0.0.27

--- include/etcpal/version.h
+++ include/etcpal/version.h
@@ -34,7 +34,7 @@
 #define ETCPAL_VERSION_MAJOR 1
 #define ETCPAL_VERSION_MINOR 0
 #define ETCPAL_VERSION_PATCH 0
-#define ETCPAL_VERSION_BUILD 26
+#define ETCPAL_VERSION_BUILD 27
 /**
  * @}
  */
@@ -43,8 +43,8 @@
  * @name EtcPal version strings
  * @{
  */
-#define ETCPAL_VERSION_STRING "1.0.0.26"
-#define ETCPAL_VERSION_DATESTR "06.Mar.2026"
+#define ETCPAL_VERSION_STRING "1.0.0.27"
+#define ETCPAL_VERSION_DATESTR "29.May.2026"
 #define ETCPAL_VERSION_COPYRIGHT "Copyright 2026 ETC Inc."
 #define ETCPAL_VERSION_PRODUCTNAME "EtcPal"
 /**
